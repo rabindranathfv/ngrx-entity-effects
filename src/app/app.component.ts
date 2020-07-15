bb261849ae5f8a201dd1b7cc173d10e26f9e61e2
Rabindranath Ferreira <ferreiralox@gmail.com>
2020-07-15 16:38:57 -0400

    add fake api with service crud for policies with ang in memory library

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PolicyService } from './service/policy.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngrx-entity-effects';
+
+  policies: any[] = [];
+  constructor(private policyService: PolicyService) { }
+
+  ngOnInit() {
+    this.policyService.getPolicies().subscribe((data : any[])=>{
+        console.log(data);
+        this.policies = data;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// ngrx
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+
+// ang in memory
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { DataService } from './fakeApi/data.service';
+
+// services
+import { PolicyService } from './service/policy.service';
 
 @NgModule({
   declarations: [
@@ -15,11 +25,13 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(DataService),
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
-  providers: [],
+  providers: [ DataService, PolicyService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
